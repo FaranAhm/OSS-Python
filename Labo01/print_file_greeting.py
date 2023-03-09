d7adea5eee47c8dabf8c5f796a43e66c9776b6be
Faran Ahmad <faran.ahmad@student.ap.be>
2023-03-09 15:28:46 +0100

    File Object: lezen -Schrijf een script print_file_greeting.py dat greeting.txt opent, en de inhoud print. -Open het bestand in read mode (zie help(open)) -Zoek de method om te lezen uit het file object en roep deze op. -Print wat je hebt gelezen. -Sluit het bestand.

---
+++ Labo01/print_file_greeting.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+file = open("greeting.txt", "r")
+
+print(file.read())
+
+file.close()
--- Labo01/write_greeting.py
+++ Labo01/write_greeting.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 file = open("greeting.txt", "w")
+
 greeting = "Hallo Faran Ahmad"
 file.write(greeting)
+
 file.close()
